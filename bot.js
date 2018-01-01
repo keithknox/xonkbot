1b2e8e4f8572a2fb21ba901bfdeec39494a2cf4b
keithknox <keithwknox@gmail.com>
2018-01-01 15:17:38 -0500

    added get and favorite functionality.

--- bot.js
+++ bot.js
@@ -4,8 +4,34 @@ var T = new Twitter(config);
 
 //set search params
 var params = {
-  q:'#nodejs',
+  q: '#nodejs',
   count: 10,
   result_type: 'recent',
   lang: 'en'
-}
+};
+
+//GET request:
+T.get('search/tweets', params, function (err, data, response) {
+  if (!err) {
+    //response once a response is returned:
+    //(clean this up later with a forEach function.)
+    //grab a tweet with the hastag "#nodejs":
+    for (let i = 0; i < data.statuses.length; i++) {
+      let id = { id: data.statuses[i].id_str };
+      //then mark it as a favorite:
+      T.post('favorites/create', id, function(err, response) {
+        if (err) {
+          console.log(err[0].message);
+        } else {
+          //find the user's screen name:
+          let username = response.user.screen_name;
+          //find the user's tweetId
+          let tweetId = response.id_str;
+          console.log('Favorited: ', 'https://twitter.com/${username}/status/${tweetId}');
+        }
+      });
+    }
+  } else {
+    console.log(err);
+  }
+});
