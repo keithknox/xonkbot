4d5ce4b608c921aa13fd515de95bcf1bdf8286b4
keithknox <keithwknox@gmail.com>
2018-01-04 22:13:55 -0500

    tweaked env's and Procfile.

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 var Twitter = require('twitter');
-// var config = require('./config.js');
-// var T = new Twitter(config);
+var config = require('./config.js');
+var T = new Twitter(config);
 
-var T = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+// var T = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// });
 
 
 //Searching and liking "#NodeJS":
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'LupmE3NqtRiJOQtcROJ8FPJFk',
-  consumer_secret: 'CCvOenzxGIk08SAPBmTWDSRpryDenx1zcB8ksu8Fbx3e0FZkTb',
+  consumer_key: 'VkP2PwcaAiqvIoWxbS2TIkrh2',
+  consumer_secret: 'JpTzFhNpX8hIUVMAPJj6cYd1ECxIG7omz5xzhEFSCjrlL81SSO',
   access_token_key: '855468487959085056-k2lzmVfspzdEnTF7183eQlRQBeDWBxe',
   access_token_secret: 'kvaI9dPaaG3MgQCdOMLgx3O14O0bvQ0om8Jmykiq3Ob7W'
 }
