8151378383ea171591506f30c5359784789fc68a
keithknox <keithwknox@gmail.com>
2018-01-04 22:06:42 -0500

    tweaked env's and Procfile.

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ var Twitter = require('twitter');
 // var config = require('./config.js');
 // var T = new Twitter(config);
 
-var T = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_SECRET
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 
