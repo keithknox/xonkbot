eba42685cd5d30a071017ee0d0fd625f8884545b
keithknox <keithwknox@gmail.com>
2018-01-04 20:52:18 -0500

    added start script and updated timer.

--- bot.js
+++ bot.js
@@ -3,33 +3,37 @@ var config = require('./config.js');
 var T = new Twitter(config);
 
 //Searching and liking "#NodeJS":
+var twitterLikes = function(){
 //set search params
-var params = {
-  q: '#nodejs',
-  count: 10,
-  result_type: 'recent',
-  lang: 'en'
-};
-
+  var params = {
+    q: '#nodejs',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+  };
 //GET request:
-T.get('search/tweets', params, function (err, data, response) {
-  if (!err) {
-    //grab a tweet with the hastag "#nodejs":
-    data.statuses.forEach(function (data) {
-      let id = { id: data.id_str };
-      T.post('favorites/create', id, function (err, response) {
-        if (err) {
-          console.log(err[0].message);
-        } else {
-          //find the user's screen name:
-          let username = response.user.screen_name;
-          //find the user's tweetId
-          let tweetId = response.id_str;
-          console.log('Favorited: ', 'https://twitter.com/${username}/status/${tweetId}');
-        }
+  T.get('search/tweets', params, function (err, data, response) {
+    if (!err) {
+      //grab a tweet with the hastag "#nodejs":
+      data.statuses.forEach(function (data) {
+        let id = { id: data.id_str };
+        T.post('favorites/create', id, function (err, response) {
+          if (err) {
+            console.log(err[0].message);
+          } else {
+            //find the user's screen name:
+            let username = response.user.screen_name;
+            //find the user's tweetId
+            let tweetId = response.id_str;
+            let date = new Date();
+            console.log('Favorited: ', 'https://twitter.com/'+username+'/status/'+tweetId+'at    '+ date);
+          }
+        });
       });
-    });
-  } else {
-    console.log(err);
-  }
-});
+    } else {
+      console.log(err);
+    }
+  });
+};
+twitterLikes();
+setInterval(twitterLikes, 1200000);
