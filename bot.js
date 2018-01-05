0b801ac8c714a8b049395d97d831d5107212b7ef
keithknox <keithwknox@gmail.com>
2018-01-04 21:24:32 -0500

    tweaked config variables

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Twitter = require('twitter');
 // var config = require('./config.js');
-var T = new Twit({
+var T = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token: process.env.TWITTER_ACCESS_TOKEN,
