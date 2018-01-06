fa5926dcfe309fb33d39d16eb54253f16d518c89
keithknox <keithwknox@gmail.com>
2018-01-06 16:16:30 -0500

    separated the like functionality from the main bot.

---
+++ twitter_likes.js
@@ -0,0 +1,38 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+module.exports =
+  //Searching and liking "#NodeJS":
+function byTweet() {
+  //set search params
+    var params = {
+      q: '#nodejs OR #NetNeutrality',
+      count: 10,
+      result_type: 'recent',
+      lang: 'en'
+    };
+  //GET request:
+    T.get('search/tweets', params, function (err, data, response) {
+      if (!err) {
+        //grab a tweet with the hastag "#nodejs":
+        data.statuses.forEach(function (data) {
+          let id = { id: data.id_str };
+          T.post('favorites/create', id, function (err, response) {
+            if (err) {
+              console.log(err[0].message);
+            } else {
+              //find the user's screen name:
+              let username = response.user.screen_name;
+              //find the user's tweetId
+              let tweetId = response.id_str;
+              let date = new Date();
+              console.log('Favorited: ', 'https://twitter.com/'+username+'/status/'+tweetId+' at '+ date);
+            }
+          });
+        });
+      } else {
+        console.log(err);
+      }
+    });
+  }
