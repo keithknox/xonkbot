f047868dbc67c2d46b0e58435fa2ad29c156e818
keithknox <keithwknox@gmail.com>
2018-01-06 22:23:52 -0500

    swapped out static phrases and add yo momma jokes!

--- bot_tweets.js
+++ bot_tweets.js
@@ -1,22 +1,16 @@
+var yoMomma = require('yo-mamma').default;
 var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
 
 module.exports = function(){
-  
-  var phraseOptions = ["beepBoop-boopBeep!","Hello World!", "im in ur tweetz.", "i am a sane genius too!", "Uniblab 4 Prez!", "lemme begin... 1/22", "a hot dog is a sandwich", "i hope you understand that i am a bot.", "bots are people too.", "social media is ez."]
-
-  function choosePhrase(arr){
-    return arr[Math.floor(Math.random() * arr.length)]
-  }
-  var phrase = choosePhrase(phraseOptions);
-
-  T.post('statuses/update', {status: phrase}, function (err, response) {
-    if (err) {
-      console.log(err[0].message);
-    } else {
-      let date = new Date();
-      console.log('Status Updated @ : ' + date);
-    }
-  });
+  var joke = yoMomma();
+    T.post('statuses/update', {status: joke}, function (err, response) {
+      if (err) {
+        console.log(err[0].message);
+      } else {
+        let date = new Date();
+        console.log('Status Updated @ : ' + date);
+      }
+    });
 }
