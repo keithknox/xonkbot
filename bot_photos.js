0ba80ac37c26560a2ca04039e83e73dd56b50a5f
keithknox <keithwknox@gmail.com>
2018-01-07 18:26:23 -0500

    added random selection functionality to my giphy searches. need to find an object to post to tweets.

--- bot_photos.js
+++ bot_photos.js
@@ -1,11 +1,26 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
-var giphy = require('giphy-wrapper')('trIEetB6DavObdvdZP4xgw5EgXjjc33e');
+var giphy = require('giphy-wrapper')(config.giphy_key);
 
-giphy.search('robots', 10, 0, 'g',function (err, data) {
+var giphyPost = giphy.search('robots', 100, 0, 'g',function (err, data) {
     if (err) {
         console.log(err)
     }
-    console.log(Object.values(data)[0].length);
+    var selectionArray = Object.values(data)[0];
+    var selection = selectionArray[Math.floor(Math.random() * selectionArray.length)]
+    // return selection.bitly_url;
+    console.log(selection)
 });
+
+ // module.exports = function(){
+ //  T.post('media/upload', {media: giphyPost}, function (err, response) {
+ //    if (err) {
+ //      console.log(err[0].message);
+ //    } else {
+ //      T.post('statuse/update', )
+ //      let date = new Date();
+ //      console.log('Status Updated @ : ' + date);
+ //    }
+ //  });
+ // }
