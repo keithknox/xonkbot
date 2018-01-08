a26f532ce772e48de627bec696139cd645376356
keithknox <keithwknox@gmail.com>
2018-01-08 17:54:16 -0500

    set the bot to run at intervals

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 var botLikes = require('./bot_likes.js');
 var botTweets = require('./bot_tweets.js');
 setInterval(botLikes, 1800000);
-// setInterval(botTweets.catFact, 1800000);
-// setInterval(botTweets.knockKnock, 3700000);
-// setInterval(botTweets.momJoke, 3800000);
-botTweets.catFact();
-botTweets.knockKnock()
-botTweets.momJoke();
+setInterval(botTweets.catFact, 1800000);
+setInterval(botTweets.knockKnock, 3700000);
+setInterval(botTweets.momJoke, 3800000);
