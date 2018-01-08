4067acc0c758230cda34325fb9f680e02fadab2d
keithknox <keithwknox@gmail.com>
2018-01-08 17:52:34 -0500

    branch cleanup.

--- bot_tweets.js
+++ bot_tweets.js
@@ -1,5 +1,4 @@
 var knockKnock = require('knock-knock-jokes');
-// var chuck = require('chuck-norris-api');
 var yoMomma = require('yo-mamma').default;
 var catFacts = require ('cat-facts');
 var Twitter = require('twitter');
@@ -9,14 +8,10 @@ var kJoke = knockKnock();
 var mJoke = yoMomma();
 var catFact = catFacts.random();
 
-// var chuckJoke = chuck.getRandom().then(function(data){
-//   console.log(data.value.joke);
-// });
-
 
 module.exports = {
   momJoke: function() {
-    T.post('statuses/update', { status: mJoke }, function (err, response) {
+    T.post('statuses/update', {status: mJoke}, function (err, response) {
       if (err) {
         console.log(err[0].message);
       } else {
