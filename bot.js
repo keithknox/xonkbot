1f716e1d3f01a7066a640bde4a970f381084e64e
keithknox <keithwknox@gmail.com>
2018-01-08 18:08:50 -0500

    added start to bot

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var botLikes = require('./bot_likes.js');
 var botTweets = require('./bot_tweets.js');
-botLikes();
+console.log("xonkbot is now live!");
 setInterval(botLikes, 1800000);
 setInterval(botTweets.catFact, 1800000);
 setInterval(botTweets.knockKnock, 3700000);
