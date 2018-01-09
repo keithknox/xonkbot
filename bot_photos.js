b3427daa2740f9e58e59a43832452eaf8cae18d6
keithknox <keithwknox@gmail.com>
2018-01-08 19:53:26 -0500

    getting ready for posting photos.

--- bot_photos.js
+++ bot_photos.js
@@ -1,26 +1,5 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
+var fs = require('fs');
+var request = require('request');
 var T = new Twitter(config);
-var giphy = require('giphy-wrapper')(config.giphy_key);
-
-var giphyPost = giphy.search('robots', 100, 0, 'g',function (err, data) {
-    if (err) {
-        console.log(err)
-    }
-    var selectionArray = Object.values(data)[0];
-    var selection = selectionArray[Math.floor(Math.random() * selectionArray.length)]
-    // return selection.bitly_url;
-    console.log(selection)
-});
-
- // module.exports = function(){
- //  T.post('media/upload', {media: giphyPost}, function (err, response) {
- //    if (err) {
- //      console.log(err[0].message);
- //    } else {
- //      T.post('statuse/update', )
- //      let date = new Date();
- //      console.log('Status Updated @ : ' + date);
- //    }
- //  });
- // }
--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
   consumer_secret: 'LeVKWhoovWM3TIHOTnnmQeOnQr4M4L86al54tiineN6lddtHXz',
   access_token_key: '855468487959085056-k2lzmVfspzdEnTF7183eQlRQBeDWBxe',
   access_token_secret: 'kvaI9dPaaG3MgQCdOMLgx3O14O0bvQ0om8Jmykiq3Ob7W',
-  giphy_key: 'trIEetB6DavObdvdZP4xgw5EgXjjc33e'
+  giphy_key: 'trIEetB6DavObdvdZP4xgw5EgXjjc33e',
+  nasa_key: 'cDnwJ5Pbw9JiNwl6SVEHS1v1cKPqknZ4ZEq9RxJw'
 };
