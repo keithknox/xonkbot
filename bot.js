84c26c8ed9884ef5d4de984d79502f9d4734d65f
keithknox <keithwknox@gmail.com>
2018-01-08 19:11:32 -0500

    cleaned up some files.

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var botLikes = require('./bot_likes.js');
 var botTweets = require('./bot_tweets.js');
+
 setInterval(botLikes, 1800000);
 setInterval(botTweets.catFact, 3600000);
 setInterval(botTweets.knockKnock, 3700000);
