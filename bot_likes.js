9a83cd53d19b6ffeff7c6049c48ba3b8e1366fae
keithknox <keithwknox@gmail.com>
2018-01-10 21:42:14 -0500

    corrected user favoriting.

--- bot_likes.js
+++ bot_likes.js
@@ -2,37 +2,60 @@ var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
 
-module.exports =
-  //Searching and liking "#NodeJS":
-function () {
-//set search params
-  var params = {
-    q: '#NodeJS OR #NetNeutrality',
-    count: 20,
-    result_type: 'recent',
-    lang: 'en'
-  };
-//GET request:
-  T.get('search/tweets', params, function (err, data, response) {
-    if (!err) {
-      //grab a tweet with the hastag "#nodejs":
-      data.statuses.forEach(function (data) {
-        let id = { id: data.id_str };
-        T.post('favorites/create', id, function (err, response) {
-          if (err) {
-            console.log(err[0].message);
-          } else {
-            //find the user's screen name:
-            let username = response.user.screen_name;
-            //find the user's tweetId
-            let tweetId = response.id_str;
-            let date = new Date();
-            console.log('Favorited: ', 'https://twitter.com/'+username+'/status/'+tweetId+' at '+ date);
-          }
+module.exports ={
+
+  hashTagLikes: function() {
+    var params = {
+      q: '#NodeJS OR #NetNeutrality',
+      count: 20,
+      result_type: 'recent',
+      lang: 'en'
+    };
+
+    T.get('search/tweets', params, function (err, data, response) {
+      if (!err) {
+        data.statuses.forEach(function (data) {
+          let id = { id: data.id_str };
+          T.post('favorites/create', id, function (err, response) {
+            if (err) {
+              console.log(err[0].message);
+            } else {
+              let username = response.user.screen_name;
+              let tweetId = response.id_str;
+              let date = new Date();
+              console.log('Favorited: ', 'https://twitter.com/'+username+'/status/'+tweetId+' at '+ date);
+            }
+          });
         });
+      } else {
+        console.log(err);
+      }
+    });
+  },
+
+  userLikes: function () {
+      var params = {
+        screen_name: "@KeithKnoxKnox",
+        count: 5
+      };
+      T.get('statuses/user_timeline', params, function (err, data, response)  {
+        if (!err) {
+          data.forEach(function (data) {
+            let id = { id: data.id_str };
+            T.post('favorites/create', id, function (err, response) {
+              if (err) {
+                console.log(err[0].message);
+              } else {
+                let username = response.user.screen_name;
+                let tweetId = response.id_str;
+                let date = new Date();
+                console.log('Favorited: ', 'https://twitter.com/'+username+'/status/'+tweetId+' at '+ date);
+              }
+            });
+          });
+        } else {
+          console.log(err);
+        }
       });
-    } else {
-      console.log(err);
     }
-  });
-};
+}
