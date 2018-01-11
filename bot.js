613f05fe1462e3b0829f76ba9c8ea6218187581c
keithknox <keithwknox@gmail.com>
2018-01-10 21:46:48 -0500

    corrected user favoriting.

--- bot.js
+++ bot.js
@@ -2,7 +2,8 @@ var botLikes = require('./bot_likes.js');
 var botTweets = require('./bot_tweets.js');
 var botReTweets = require('./bot_retweets.js');
 
-setInterval(botLikes, 1800000);
+setInterval(botLikes.hashTagLikes, 1800000);
+setInterval(botLikes.userLikes, 1800000);
 setInterval(botReTweets, 1800000);
 setInterval(botTweets.catFact, 3600000);
 setInterval(botTweets.knockKnock, 3700000);
