91e08816a98ceaf6fe21878ba4b7a05893ab3e85
keithknox <keithwknox@gmail.com>
2018-02-04 18:03:49 -0500

    finished nasaPic function to post daily NASA images

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var botLikes = require('./bot_likes.js');
 var botTweets = require('./bot_tweets.js');
 var botReTweets = require('./bot_retweets.js');
+var botPhotos = require('./bot_photos.js');
 
 setInterval(botLikes.hashTagLikes, 1800000);
 setInterval(botLikes.userLikes, 1800000);
@@ -8,5 +9,6 @@ setInterval(botReTweets, 1800000);
 setInterval(botTweets.catFact, 3600000);
 setInterval(botTweets.knockKnock, 3700000);
 setInterval(botTweets.momJoke, 3800000);
-setInterval(botTweets.chuckJoke, 1900000)
+setInterval(botTweets.chuckJoke, 1900000);
+setInterval(botPhotos.nasaPic, 86400000);
 console.log("xonkbot is now live!");
--- bot_photos.js
+++ bot_photos.js
@@ -1,5 +1,45 @@
-var Twitter = require('twitter');
-var config = require('./config.js');
+var axios = require('axios');
 var fs = require('fs');
-var request = require('request');
+var Twitter = require('twitter');
+var config = require('./config');
 var T = new Twitter(config);
+
+module.exports = {
+  nasaPic: function () {
+    //getting the image
+    axios.get('https://api.nasa.gov/planetary/apod?api_key='+config.nasa_key).then(function (response) {
+        var imageSave = axios({
+          method: 'get',
+          url: response.data.url,
+          responseType: 'stream'
+        }).then(function(response){
+          response.data.pipe(fs.createWriteStream('nasa.jpg'));
+          console.log('File saved!');
+        });
+        //namting the title
+        var title = response.data.title;
+
+        //loading the image to tweet
+        var image = fs.readFileSync('nasa.jpg');
+        T.post('media/upload', { media: image }, function (error, media, response) {
+          if (!error) {
+            console.log('Image successfully loaded.');
+            var status = {
+              status: "Today's NASA Pic of the Day: " + title,
+              media_ids: media.media_id_string // Pass the media id string
+            }
+
+          //taking the image, and tweet and posting the tweet
+            T.post('statuses/update', status, function (error, tweet, response) {
+              if (!error) {
+                tweet;
+              }
+            });
+          }
+        });
+
+      }).catch(function(error){
+          console.log('Error: ' + error);
+    });
+    }
+}
