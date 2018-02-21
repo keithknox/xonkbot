08703f318c6138ae915e363f80cb6920fbdcfaa4
keithknox <keithwknox@gmail.com>
2018-02-20 19:19:21 -0500

    finished gif functionality. pushing to the bot!

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var byHashTag = require('./bot_likes/by_hash_tag.js');
 var byUser = require('./bot_likes/by_user.js');
 var reTweets = require('./bot_retweets/retweets.js');
 var nasaPic = require('./bot_photos/nasa_pic.js');
+var roboGifs = require('./bot_gifs/robots.js');
 
 setInterval(catFact, 3800000);
 setInterval(knockKnock, 3800000);
@@ -15,5 +16,6 @@ setInterval(byHashTag, 1800000);
 setInterval(byUser, 1800000);
 setInterval(reTweets, 1800000);
 setInterval(nasaPic, (86400000 / 2));
+setInterval(roboGifs, 7200000);
 
 console.log('xonkbot is now live!');
--- bot_gifs/robots.js
+++ bot_gifs/robots.js
@@ -1,10 +1,68 @@
 var request = require('request');
 var fs = require('fs');
+var pathToMovie = './tmp/robots.gif';
+var mediaType = 'image/gif';
+var mediaData = fs.readFileSync(pathToMovie);
+var mediaSize = fs.statSync(pathToMovie).size;
 var Twitter = require('twitter');
 var config = require('../config.js');
 var T = new Twitter(config);
 
-function robotsGif() {
+module.exports = function () {
+    initUpload()
+    .then(appendUpload)
+    .then(finalizeUpload)
+    .then(mediaId => {
+        var status = {
+          status: "I'd like to  share some family photos: ",
+          media_ids: mediaId,
+        };
+        T.post('statuses/update', status, function (err, tweet, res) {
+          if (!err) {
+            var date = new Date();
+            console.log('Status Updated with Robot GIF @ : ' + date);
+          }
+        });
+      }).then(getRobotsGif);
+  };
+
+function initUpload() {
+  return makePost('media/upload', {
+    command: 'INIT',
+    total_bytes: mediaSize,
+    media_type: mediaType,
+  }).then(data => data.media_id_string);
+}
+
+function appendUpload (mediaId) {
+  return makePost('media/upload', {
+    command: 'APPEND',
+    media_id: mediaId,
+    media: mediaData,
+    segment_index: 0
+  }).then(data => mediaId);
+}
+
+function finalizeUpload(mediaId) {
+  return makePost('media/upload', {
+    command: 'FINALIZE',
+    media_id: mediaId
+  }).then(data => mediaId);
+}
+
+function makePost(endpoint, params) {
+  return new Promise((resolve, reject) => {
+    T.post(endpoint, params, (error, data, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+function getRobotsGif() {
   request.get('http://api.giphy.com/v1/gifs/random?tag=robots&api_key='+ config.giphy_key, function(err, res, body) {
     body = JSON.parse(body);
     gif = body.data.images.fixed_height;
